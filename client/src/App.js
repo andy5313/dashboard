ba40826027a0c6b3c3d18658592209d6932d265b
andyz5313 <Your Emailand>
2023-03-22 13:34:55 -0400

    client and server

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "theme";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "scenes/dashboard";
+import Layout from "scenes/layout";
+
+function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route
+                path="/"
+                element={<Navigate to="/dashboard" replace />}
+              ></Route>
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
